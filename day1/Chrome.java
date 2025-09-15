17d88b59f9345d47d1767e4601be225cafd882f8
raziabanu22 <1996raziabanu@gmail.com>
2025-09-15 19:04:49 +0530

    Add files via upload

---
+++ day1/Chrome.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+		System.out.println(version + " " + developer + " " + isBeta + " " + releaseYear + " " + shortcutKey);
+
+	}
+
+}
